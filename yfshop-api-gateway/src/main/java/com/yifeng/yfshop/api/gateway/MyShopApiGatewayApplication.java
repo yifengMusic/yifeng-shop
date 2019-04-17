cd6cbb91586c599ddba6d9116abf91758c02cabb
yifeng <9282207@qq.com>
2019-04-17 17:33:13 +0800

    API网关

---
+++ yfshop-api-gateway/src/main/java/com/yifeng/yfshop/api/gateway/MyShopApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.yifeng.yfshop.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrix
+@EnableHystrixDashboard
+@SpringBootApplication(scanBasePackages = "com.yifeng.yfshop", exclude = DataSourceAutoConfiguration.class)
+public class MyShopApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyShopApiGatewayApplication.class, args);
+    }
+}
