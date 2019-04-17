b5e57b6e81563175fda9a4d4274c1cd4a2cf36a9
yifeng <9282207@qq.com>
2019-04-17 17:52:27 +0800

    搜索服务消费者

---
+++ yfshop-service-search-consumer/src/main/java/com/yifeng/yfshop/service/search/consumer/MyShopServiceSearchConsumerApplication.java
@@ -0,0 +1,17 @@
+package com.yifeng.yfshop.service.search.consumer;
+import com.alibaba.dubbo.container.Main;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrix
+@EnableHystrixDashboard
+@SpringBootApplication(scanBasePackages = "com.yifeng.yfshop", exclude = DataSourceAutoConfiguration.class)
+public class MyShopServiceSearchConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyShopServiceSearchConsumerApplication.class, args);
+        Main.main(args);
+    }
+}
