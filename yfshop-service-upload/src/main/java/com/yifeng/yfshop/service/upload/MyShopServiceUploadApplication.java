e1df8f0ca9a2253dd9801b98c470d8d67bb6930b
yifeng <9282207@qq.com>
2019-04-17 17:53:47 +0800

    FastDFS分布式文件系统服务提供者

---
+++ yfshop-service-upload/src/main/java/com/yifeng/yfshop/service/upload/MyShopServiceUploadApplication.java
@@ -0,0 +1,12 @@
+package com.yifeng.yfshop.service.upload;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShopServiceUploadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyShopServiceUploadApplication.class, args);
+    }
+}
