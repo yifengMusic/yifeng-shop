9d561ef1f324c4e12577a5cce734255c1d0d5851
yifeng <9282207@qq.com>
2019-04-17 17:56:53 +0800

    api网关(实际使用这个)

---
+++ yfshop-web-admin/src/main/java/com/yifeng/yfshop/web/admin/MyShopWebAdminApplication.java
@@ -0,0 +1,16 @@
+package com.yifeng.yfshop.web.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrix
+@EnableHystrixDashboard
+@SpringBootApplication(scanBasePackages = "com.yifeng.yfshop", exclude = DataSourceAutoConfiguration.class)
+public class MyShopWebAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyShopWebAdminApplication.class, args);
+    }
+}
