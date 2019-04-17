cc8303c20ef4961bb0f5f7a75f088554f135c05a
yifeng <9282207@qq.com>
2019-04-17 17:49:08 +0800

    缓存服务api

---
+++ yfshop-service-redis-api/src/main/java/com/yifeng/yfshop/service/redis/api/RedisService.java
@@ -0,0 +1,11 @@
+package com.yifeng.yfshop.service.redis.api;
+
+public interface RedisService {
+    void set(String key, Object value);
+
+    void set(String key, Object value, int seconds);
+
+    void del(String key);
+
+    Object get(String key);
+}
